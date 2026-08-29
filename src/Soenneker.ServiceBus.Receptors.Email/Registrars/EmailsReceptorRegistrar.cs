708df01d1b09d06b1f8a2d161fba372477cd23de
soenneker <jake@soenneker.com>
2026-08-29 14:17:20 -0500

    Improve public API XML documentation

--- src/Soenneker.ServiceBus.Receptors.Email/Registrars/EmailsReceptorRegistrar.cs
+++ src/Soenneker.ServiceBus.Receptors.Email/Registrars/EmailsReceptorRegistrar.cs
@@ -13,6 +13,8 @@ public static class EmailsReceptorRegistrar
     /// <summary>
     /// Adds <see cref="IEmailsReceptor"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddEmailsReceptorAsSingleton(this IServiceCollection services)
     {
         services.AddServiceBusQueueUtilAsSingleton().TryAddSingleton<IEmailsReceptor, EmailsReceptor>();
